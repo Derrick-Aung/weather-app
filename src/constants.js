c4426e5ff1f6e900ef691681968db11becf57924
AungPhoneKhant <akha0030@student.monash.edu>
2020-07-31 13:14:25 +1000

    Removed unnecessary files

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const WEATHER_API_HOST = "https://api.openweathermap.org/data/2.5/";
+export const WEATHER_ICON_API_HOST = (weather_code) =>
+    `openweathermap.org/img/wn/${weather_code}@2x.png`;
