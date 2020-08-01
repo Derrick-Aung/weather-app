55fed1f22be2f72b9450caa142422310ea20d3fe
AungPhoneKhant <akha0030@student.monash.edu>
2020-08-01 14:12:36 +1000

    Implemented connection to backend for search query

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,20 @@
 import axios from "axios";
-import { WEATHER_API_HOST, WEATHER_ICON_API_HOST } from "../constants";
+import {
+    BACKEND_HOST,
+    WEATHER_API_HOST,
+    WEATHER_ICON_API_HOST,
+} from "../constants";
 import {
     GET_WEATHER_INITIATED,
     GET_WEATHER_FAIL,
     GET_WEATHER_SUCCESS,
 } from "../types";
+
+const config = {
+    headers: {
+        "Content-Type": "application/json",
+    },
+};
 
 export const getWeatherData = (city_name) => async (dispatch) => {
     const weather_url = `${WEATHER_API_HOST}?appid=${process.env.REACT_APP_OPEN_WEATHER_KEY}&q=${city_name}`;
@@ -12,6 +22,9 @@ export const getWeatherData = (city_name) => async (dispatch) => {
     dispatch({
         type: GET_WEATHER_INITIATED,
     });
+
+    // send search query to node backend
+    axios.post(`${BACKEND_HOST}searches`, { text: city_name }, config);
 
     try {
         const { data } = await axios.get(weather_url);
